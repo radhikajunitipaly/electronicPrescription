a03683e0f09293db3f7406868c7bf1b257d75c59
Ragav-Charan <charan.ragav@gmail.com>
2018-11-21 20:12:26 -0600

    View Prescription Activity class

--- app/src/main/java/com/uta/eprescription/activities/prescMgr/common/ViewPrescriptionActivity.java
+++ app/src/main/java/com/uta/eprescription/activities/prescMgr/common/ViewPrescriptionActivity.java
@@ -1,5 +1,18 @@
 package com.uta.eprescription.activities.prescMgr.common;
 
-public class ViewPrescriptionActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
 
+import com.uta.eprescription.R;
+
+public class ViewPrescription extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_prescription);
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_vw_status);
+        spinner.setEnabled(false);
+    }
 }
